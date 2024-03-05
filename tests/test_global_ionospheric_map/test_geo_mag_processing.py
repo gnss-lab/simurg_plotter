15e65c807704c790ce34a6cd3da292214c111e67
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2024-03-05 21:36:34 +0800

    add test_get_uniform_mag_net

--- tests/test_global_ionospheric_map/test_geo_mag_processing.py
+++ tests/test_global_ionospheric_map/test_geo_mag_processing.py
@@ -1,14 +1,18 @@
 import pytest
+import numpy as np
 from simurg_plotter.global_ionospheric_map.geo_mag_processing import get_gm_contours, get_uniform_mag_net, gims_limits, prepare_contours
+
+def test_get_uniform_mag_net():
+    result = get_uniform_mag_net()
+
+    assert isinstance(result, np.ndarray)
+    assert result.shape != (0, 0)
+
+def test_get_gm_contours():
+    pass
 
 def test_prepare_contours():
     pass
 
 def test_gims_limits():
-    pass
-
-def test_get_uniform_mag_net():
-    pass
-
-def test_get_gm_contours():
     pass
