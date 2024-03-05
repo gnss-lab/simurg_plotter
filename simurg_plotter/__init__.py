4d948269070873ca766e9701150da17c7ba0bb85
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2024-03-05 20:20:06 +0800

    update __init__.py

--- simurg_plotter/__init__.py
+++ simurg_plotter/__init__.py
@@ -1,1 +1,1 @@
-from .global_ionospheric_map import visualization
+from .global_ionospheric_map import visualization, geo_mag_processing
--- simurg_plotter/global_ionospheric_map/__init__.py
+++ simurg_plotter/global_ionospheric_map/__init__.py
@@ -1,1 +1,2 @@
 from .visualization import plot
+from .geo_mag_processing import get_gm_contours, get_uniform_mag_net, gims_limits, prepare_contours
