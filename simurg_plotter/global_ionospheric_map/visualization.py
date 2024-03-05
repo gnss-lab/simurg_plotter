0b50c2c3f7fd2fa980a648ae7ed89af1bee1c519
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2024-03-05 20:39:01 +0800

    formatted code

--- simurg_plotter/global_ionospheric_map/visualization.py
+++ simurg_plotter/global_ionospheric_map/visualization.py
@@ -9,13 +9,17 @@ from typing import List, Optional
 
 WITH_MAP = -1  # this needed because of imshow()
 
-font = {'family': 'sans-serif',
-        "sans-serif": "DejaVu Sans",
-        'size': 14}
+font = {"family": "sans-serif", "sans-serif": "DejaVu Sans", "size": 14}
 
-matplotlib.rc('font', **font)
+matplotlib.rc("font", **font)
 
-def plot(gims: List[np.ndarray], fig_path: Optional[List[str]] = [], geo: bool = True, **kwargs: Optional[dict]):
+
+def plot(
+    gims: List[np.ndarray],
+    fig_path: Optional[List[str]] = [],
+    geo: bool = True,
+    **kwargs: Optional[dict],
+):
     """Plot Geo-Magnetic maps.
 
     :param gims: List of maps to plot
@@ -38,21 +42,23 @@ def plot(gims: List[np.ndarray], fig_path: Optional[List[str]] = [], geo: bool =
     :type **kwargs: Optional[dict]
     """
     if len(fig_path) != 0 and len(gims) != len(fig_path):
-        msg = f'Path to figures must be provide for each of {len(gims)} maps'
+        msg = f"Path to figures must be provide for each of {len(gims)} maps"
         raise ValueError(msg)
-    width = kwargs.get('width', 3000)
-    height = kwargs.get('width', 1500)
-    dpi = kwargs.get('dpi', 300)
-    lonlat = prepare_contours(geo, kwargs.get('gm_contours', get_gm_contours()))
+    width = kwargs.get("width", 3000)
+    height = kwargs.get("width", 1500)
+    dpi = kwargs.get("dpi", 300)
+    lonlat = prepare_contours(
+        geo, kwargs.get("gm_contours", get_gm_contours())
+    )
     mag_geo_net = get_uniform_mag_net()
     lims = gims_limits(gims)
     for igim, arr in enumerate(gims):
-        fig = plt.figure(figsize=(width/dpi, height/dpi), dpi=dpi)
+        fig = plt.figure(figsize=(width / dpi, height / dpi), dpi=dpi)
         plot_ax = fig.add_subplot(111)
-        plot_ax.plot(lonlat[:, 0], lonlat[:, 1], color='black', alpha=0.5)
+        plot_ax.plot(lonlat[:, 0], lonlat[:, 1], color="black", alpha=0.5)
         im = None
         if WITH_MAP == -1:
-            if kwargs.get('only_contours', False) == True:
+            if kwargs.get("only_contours", False) == True:
                 arr[:, :] = None
             if arr.shape[1] % 2 != 0:
                 im = plot_ax.imshow(arr, aspect=0.5)
@@ -61,13 +67,13 @@ def plot(gims: List[np.ndarray], fig_path: Optional[List[str]] = [], geo: bool =
                 arr1[:, : arr1.shape[1] - 1] = arr
                 arr1[:, -1] = arr[:, 0]
                 im = plot_ax.imshow(arr1, aspect=0.5)
-        if 'titles' in kwargs:
-            if len(kwargs['titles']) < igim:
+        if "titles" in kwargs:
+            if len(kwargs["titles"]) < igim:
                 pass
             else:
-                fig.suptitle(kwargs['titles'][igim])
+                fig.suptitle(kwargs["titles"][igim])
         else:
-            plot_ax.axis('off')
+            plot_ax.axis("off")
         fig.tight_layout()
         yt = [0] + list(range(5, 70, 6)) + [70]
         yl = [87.5] + list(range(75, -76, -15)) + [-87.5]
@@ -79,11 +85,11 @@ def plot(gims: List[np.ndarray], fig_path: Optional[List[str]] = [], geo: bool =
         plot_ax.set_xticklabels(xl)
         if im:
             im.set_clim(vmin=lims[0], vmax=lims[1])
-        fig.colorbar(im, label='TEC, TECu', fraction=0.07, pad=0.035)
-        if 'cmin' in kwargs and 'cmax'in kwargs:
-            plot_ax.set_clim(kwargs['cmin'], kwargs['cmax'])
-        if 'vmin' in kwargs and 'vmax' in kwargs:
-            plot_ax.set_clim(vmin=kwargs['vmin'], vmax=kwargs['vmax'])
+        fig.colorbar(im, label="TEC, TECu", fraction=0.07, pad=0.035)
+        if "cmin" in kwargs and "cmax" in kwargs:
+            plot_ax.set_clim(kwargs["cmin"], kwargs["cmax"])
+        if "vmin" in kwargs and "vmax" in kwargs:
+            plot_ax.set_clim(vmin=kwargs["vmin"], vmax=kwargs["vmax"])
         if len(fig_path) != len(gims):
             mng = plt.get_current_fig_manager()
             mng.full_screen_toggle()
