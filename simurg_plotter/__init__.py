9a4ca7b749c862696f64a8c51871b963fc7a0a68
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2024-04-02 11:56:07 +0800

    add map2d

--- simurg_plotter/__init__.py
+++ simurg_plotter/__init__.py
@@ -1,2 +1,3 @@
 from .global_ionospheric_map import visualization, geo_mag_processing
 from .ionospheric_pierce_point import visualization
+from .map2d import map2d
--- simurg_plotter/ionospheric_pierce_point/__init__.py
+++ simurg_plotter/ionospheric_pierce_point/__init__.py
@@ -1,7 +1,6 @@
 import numpy as np
 from dataclasses import dataclass
 
-
 @dataclass
 class Series:
     """
