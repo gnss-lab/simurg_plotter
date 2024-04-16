59a96535c91137e0c3996ac8f0ae857b5b798752
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2024-04-16 16:51:17 +0800

    Update __init__.py

--- simurg_plotter/__init__.py
+++ simurg_plotter/__init__.py
@@ -1,3 +1,4 @@
 from .global_ionospheric_map import visualization, geo_mag_processing
 from .ionospheric_pierce_point import visualization
 from .map2d import map2d
+from manager import PlotManager, Plots
